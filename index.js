b658022d7f4260c62f527d54cefa59bc1d1d49aa
Ramiro <63085235+Ciberramir52@users.noreply.github.com>
2024-02-27 22:17:40 -0500

    Make fundamental part of the Project

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const containerDiv = document.querySelector("#container");
-for (let i = 0; i < 4; i++){
-    const column = document.createElement("div");
-    column.className = "column";
+const buttonGrid = document.querySelector("#button-grid");
 
-    for (let j = 0; j < 4; j++) {
-        const row = document.createElement("div");
-        row.className = "row";
-        column.appendChild(row);
+buttonGrid.addEventListener('click', () => changeGrid());
+
+const createGrid = (side) => {
+    for (let i = 0; i < side; i++){
+        const column = document.createElement("div");
+        column.className = "column";
+    
+        for (let j = 0; j < side; j++) {
+            const row = document.createElement("div");
+            row.className = "row";
+            row.addEventListener('mouseover', e => e.target.style.backgroundColor = 'red');
+            column.appendChild(row);
+        }
+        containerDiv.appendChild(column);
     }
-    containerDiv.appendChild(column);
 }
+
+const changeGrid = () => {
+    let side = Number(prompt("What will be the new side of the grid?"));
+    console.log(side);
+    while(side > 100) {
+        console.log(side);
+        side = Number(prompt("What will be the new side of the grid?"));
+    }
+    if(side){
+        deleteChildren();
+        createGrid(side);
+    }
+}
+
+const deleteChildren = () => {
+    let childColumn = containerDiv.lastElementChild;
+    while(childColumn){
+        let childRow = childColumn.lastElementChild;
+        while(childRow){
+            childColumn.removeChild(childRow);
+            childRow = childColumn.lastElementChild;
+        }
+        containerDiv.removeChild(childColumn);
+        childColumn = containerDiv.lastElementChild;
+    }
+}
+
+createGrid(16);
